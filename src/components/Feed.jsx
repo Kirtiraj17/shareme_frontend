47f533fb61938cdfcaec4a668b6b32436e7ecf1f
gkirtirajprdxn <kirtirajg@axioned.com>
2023-01-19 18:02:10 +0530

    UserDetail component added with some refactor

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -15,31 +15,25 @@ const Feed = () => {
     if (categoryId) {
       setLoading(true);
       const query = searchQuery(categoryId);
-      client
-        .fetch(query)
-        .then((data) => {
-          setPins(data);
-          setLoading(false);
-        });
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
     } else {
       setLoading(true);
-      client
-        .fetch(feedQuery)
-        .then((data) => {
-          setPins(data);
-          setLoading(false);
-        });
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
     }
   }, [categoryId]);
 
   if (loading)
     return <Spinner message="We are adding new ideas to your feed!" />;
 
-  return (
-    <div>
-      {pins && <MasonryLayout pins={pins} />}
-    </div>
-  );
+  if (!pins?.length) return <h2>No pins available!</h2>;
+
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
