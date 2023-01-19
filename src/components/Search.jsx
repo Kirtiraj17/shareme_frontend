1f64b8c119d7c5a58c974118826e5f6ed2792643
gkirtirajprdxn <kirtirajg@axioned.com>
2023-01-19 18:29:40 +0530

    code refactor

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,26 @@
-import React, {useState, useEffect} from 'react'
-import { client } from '../client'
-import { feedQuery, searchQuery } from '../utils/data'
-import MasonryLayout from './MasonryLayout'
-import Spinner from './Spinner'
+import React, { useState, useEffect } from "react";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
-const Search = ({searchTerm}) => {
+const Search = ({ searchTerm }) => {
   const [pins, setPins] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if(searchTerm) {
-      setLoading(true)
-      const query = searchQuery(searchTerm.toLowerCase())
-      client.fetch(query)
-        .then((data)=> {
-          setPins(data)
-          setLoading(false)
-        })
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
     } else {
-      client.fetch(feedQuery)
-        .then((data)=> {
-          setPins(data)
-          setLoading(false)
-        })
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
     }
   }, [searchTerm]);
 
@@ -30,13 +28,11 @@ const Search = ({searchTerm}) => {
     <div>
       {loading && <Spinner message="Searching for pins..." />}
       {pins?.length !== 0 && <MasonryLayout pins={pins} />}
-      {pins?.length === 0 && searchTerm !== '' && !loading && (
-        <div className='mt-10 text-center text-xl'>
-          No Pins Found!
-        </div>
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">No Pins Found!</div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
